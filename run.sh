9bd8d9c081e4cce7c7a697852f7502a08ad48069
Michele Colombo <mikexine@gmail.com>
2017-02-23 12:41:23 +0100

    improve run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+cd /home/twitter/trump-twitter
+source env/bin/activate
 curl -o realdonaldtrump.zip https://raw.githubusercontent.com/bpb27/political_twitter_archive/master/realdonaldtrump/realdonaldtrump_long.zip
 python load_archive.py
