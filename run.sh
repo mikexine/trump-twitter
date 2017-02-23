e9e4371970fa69d10fa684a0d613911e737f7ba0
Michele Colombo <mikexine@gmail.com>
2017-02-23 12:54:12 +0100

    tentative

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-source /home/twitter/trump-twitter/env/bin/activate
+source "/home/twitter/trump-twitter/env/bin/activate"
 curl -o /home/twitter/trump-twitter/realdonaldtrump.zip https://raw.githubusercontent.com/bpb27/political_twitter_archive/master/realdonaldtrump/realdonaldtrump_long.zip
 python /home/twitter/trump-twitter/load_archive.py
